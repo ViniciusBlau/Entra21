87fbc3469fcaabfee4f9cfc88f37c46c6e3416e2
ViniciusBlau <108106730+ViniciusBlau@users.noreply.github.com>
2023-09-29 14:44:38 -0300

    Update Principal.java

--- Classe06/src/Classe06/Principal.java
+++ Classe06/src/Classe06/Principal.java
@@ -1,13 +1,10 @@
-package Classe06;
+package exercicio;
 
 public class Principal {
 
 	public static void main(String[] args) {
-
 		/*
-		 * Utilizando a classe Mercado.
-		 * 
-		 * Atribua valores às variáveis da classe Mercado para cada um dos objetos de
+		 * tribua valores às variáveis da classe Mercado para cada um dos objetos de
 		 * Mercado.
 		 * 
 		 * Sendo que o preço de compra dos produtos foi igual em todos os mercados.
@@ -19,17 +16,63 @@ public class Principal {
 		 * maior vendendo maçãs ou laranjas?
 		 */
 
-		Mercado unidadeDeBlumenau = new Mercado(1000, 5.20, 870, 4.35, "Blumenau");
+		String menosLucro = "";
+		String maisLucro = "";
+		String locMaiorMacas = "";
+		String locMaiorLaranjas = "";
+		String locMeio = "";
 
-		Mercado unidadeDeJoinville = new Mercado(1200, 4.90, 800, 4.75, "Joinville");
+		double maiorMacas = Integer.MIN_VALUE;
+		double maiorLaranjas = Integer.MIN_VALUE;
+		double maiorLucro = Integer.MIN_VALUE;
+		double menorLucro = Integer.MAX_VALUE;
+		double valorMeio = Integer.MIN_VALUE;
+		double valorTotal = 0;
 
-		Mercado unidadeDeFlorianopolis = new Mercado(1400, 5, 1200, 5.0, "Floripa");
-		
+		Mercado unidadeDeBlumenau = new Mercado(800, 5, 800, 5, "Blumenau");
+		Mercado unidadeDeJoiville = new Mercado(900, 5, 950, 5, "Joinville");
+		Mercado unidadeDeFloripa = new Mercado(1000, 5, 800, 5, "Florianópolis");
+
 		Mercado vetor[] = new Mercado[3];
 		vetor[0] = unidadeDeBlumenau;
-		vetor[1] = unidadeDeJoinville;
-		vetor[2] = unidadeDeFlorianopolis;
+		vetor[1] = unidadeDeJoiville;
+		vetor[2] = unidadeDeFloripa;
 
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i].lucroMacas() > maiorMacas) {
+				maiorMacas = vetor[i].lucroMacas();
+				locMaiorMacas = vetor[i].getLocalidade();
+			}
+			if (vetor[i].lucroLaranjas() > maiorLaranjas) {
+				maiorLaranjas = vetor[i].lucroLaranjas();
+				locMaiorLaranjas = vetor[i].getLocalidade();
+			}
+			if (vetor[i].lucroTotal() > maiorLucro) {
+				maiorLucro = vetor[i].lucroTotal();
+				maisLucro = vetor[i].getLocalidade();
+			}
+			if (vetor[i].lucroTotal() < menorLucro) {
+				menorLucro = vetor[i].lucroTotal();
+				menosLucro = vetor[i].getLocalidade();
+			}
+			if (vetor[i].lucroTotal() > valorMeio && vetor[i].lucroTotal() < maiorLucro) {
+				valorMeio = vetor[i].lucroTotal();
+				locMeio = vetor[i].getLocalidade();
+			}
+
+		}
+		
+		for (int i = 0; i < vetor.length; i++) {
+			valorTotal += vetor[i].lucroTotal(); 
+		}
+
+		System.out.println("A franquia que mais lucrou com maçãs: " + locMaiorMacas);
+		System.out.println("A franquia que mais lucrou com laranjas: " + locMaiorLaranjas);
+		System.out.println("A franquia que mais lucrou ao todo: " + maisLucro);
+		System.out.println("A franquia que menos lucrou ao todo: " + menosLucro);
+		System.out.println("A franquia que ficou no meio: " + locMeio);
+		System.out.println("O lucro total das franquias: " + valorTotal);
 
 	}
+
 }
