2bd270c76ae93af496e0772aff1bd1d33833d7a8
ViniciusBlau <108106730+ViniciusBlau@users.noreply.github.com>
2023-11-09 18:39:06 -0300

    Update Principal.java

--- TrabalhoJava/src/TrabalhoJava/Principal.java
+++ TrabalhoJava/src/TrabalhoJava/Principal.java
@@ -1,6 +1,5 @@
 package TrabalhoJava;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principal {
@@ -10,26 +9,91 @@ public class Principal {
 
 		Endereco endereco01 = new Endereco("Rua São Paulo", "Blumenau", "SC", "Brasil", 1254);
 
-		Cinema cinema01 = new Cinema("dinheiro", endereco01, true, 1, "Bebelo", "comédia");
+		System.out.println("  ______________________________");
+		System.out.println("  |                            |");
+		System.out.println("  |      Filmes em Cartaz      |");
+		System.out.println("  |____________________________|");
+		System.out.println("  |                            |");
+		System.out.println("  |  1   Piratas do Caribe     |"); // ação
+		System.out.println("  |  2   Gente grande 2        |"); // comédia
+		System.out.println("  |  3   Titanic               |"); // drama
+		System.out.println("  |  4   Amor em redenção      |"); // romance
+		System.out.println("  |  5   Sorria                |"); // terror
+		System.out.println("  |  6   22 de Julho           |"); // documentário
+		System.out.println("  |  7   Nós                   |"); // suspense
+		System.out.println("  |  8   Interestelar          |"); // ficção científica
+		System.out.println("  |____________________________|"); //
+		System.out.println();
 
-		System.out.println("Digite o assento que deseja reservar(A1 - D4): ");
-		int assento = entrada.nextInt();
-		cinema01.reserva(assento);
+		System.out.println("Digite o número do filme de sua preferência: ");
+		int escolhaFilme = entrada.nextInt();
 
-		while (assento != 0) {
+		String generoFilme = "";
+		String nomeFilme = "";
 
-			System.out.println("Digite o assento que deseja reservar(A1 - D4), ou 'sair' para encerrar: ");
-			assento = entrada.nextInt();
-			if (assento > 31 || assento < 0) {
-				System.out.println("Assento inválido!");
-			} else {
-				cinema01.reserva(assento);
+		while (true) {
+			switch (escolhaFilme) {
+			case 1:
+				nomeFilme = "Piratas do Caribe";
+				generoFilme = "Ação";
+				break;
+			case 2:
+				nomeFilme = "Gente Grande 2";
+				generoFilme = "Comédia";
+				break;
+			case 3:
+				nomeFilme = "Titanic";
+				generoFilme = "Drama";
+				break;
+			case 4:
+				nomeFilme = "Amor em Redenção";
+				generoFilme = "Romance";
+				break;
+			case 5:
+				nomeFilme = "Sorria";
+				generoFilme = "Terror";
+				break;
+			case 6:
+				nomeFilme = "22 de Julho";
+				generoFilme = "Documentário";
+				break;
+			case 7:
+				nomeFilme = "Nós";
+				generoFilme = "Suspense";
+				break;
+			case 8:
+				nomeFilme = "Interestelas";
+				generoFilme = "Ficção Científica";
+				break;
+			default:
+				System.out.println("Filme inválido!");
+				
 			}
+			
+			if (escolhaFilme >= 1 && escolhaFilme <= 8) {
+		        break;
+		    }
+			System.out.println("Digite o número do filme de sua preferência: ");
+			escolhaFilme = entrada.nextInt();
+			
+		}
+		
+		Cinema cinema01 = new Cinema("dinheiro", endereco01, true, 1, nomeFilme, generoFilme);
 
+		System.out.println("Quantos ingressos deseja comprar? ");
+		int qtdIngressos = entrada.nextInt();
+
+		while (qtdIngressos < 1 || qtdIngressos > 30) {
+			System.out.println("Quantidade de ingressos inválida, tente novamente!");
+			System.out.println("Quantos ingressos deseja comprar? ");
+			qtdIngressos = entrada.nextInt();
 		}
 
+		
+		cinema01.reservar(qtdIngressos);
 		System.out.println(cinema01.toString());
-
+		
+		entrada.close();
 	}
 
 }
