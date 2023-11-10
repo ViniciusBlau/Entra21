7db001df21e0a74306b199a10702103908a2a777
ViniciusBlau <108106730+ViniciusBlau@users.noreply.github.com>
2023-11-09 21:13:41 -0300

    Update Lanchonete.java

--- TrabalhoJava/src/TrabalhoJava/Lanchonete.java
+++ TrabalhoJava/src/TrabalhoJava/Lanchonete.java
@@ -1,62 +1,102 @@
 package TrabalhoJava;
 
-public class Lanchonete extends Estabelecimento {
+import java.util.Scanner;
 
-	private String alimento;
-	private String bebida;
-	private boolean pagamento;
+public class Lanchonete extends Estabelecimento {
+	Scanner entrada = new Scanner(System.in);
+
+	private int funcionarios;
 
 	public Lanchonete(String formaPagamento, Endereco local, boolean funcionamento, int saidasEmergencia,
-			String alimento, String bebida, String pagamento) {
+			int funcionarios) {
 		super(formaPagamento, local, funcionamento, saidasEmergencia);
-		setAlimento(alimento);
-		setBebida(bebida);
-		setPagamento(funcionamento);
+		setFuncionarios(funcionarios);
 	}
 
-	public String getAlimento() {
-		return alimento;
+	public int getFuncionarios() {
+		return funcionarios;
 	}
 
-	public void setAlimento(String alimento) {
-		if (alimento == null || alimento.isBlank()) {
-			throw new IllegalArgumentException("Alimento inválido!");
+	public void setFuncionarios(int funcionarios) {
+		if (funcionarios < 1 || funcionarios > 15) {
+			throw new IllegalArgumentException("Funcionários inválidos!");
 		}
-		this.alimento = alimento;
+		this.funcionarios = funcionarios;
 	}
 
-	public String getBebida() {
-		return bebida;
-	}
+	public void comprar() {
 
-	public void setBebida(String bebida) {
-		if (bebida == null || bebida.isBlank()) {
-			throw new IllegalArgumentException("Bebida inválida!");
+		System.out.println("  ________________________________________");
+		System.out.println("  |                                      |");
+		System.out.println("  |         Alimentos e Bebidas          |");
+		System.out.println("  |______________________________________|");
+		System.out.println("  |                                      |");
+		System.out.println("  |  1   Pipoca Salgada       R$ 10.00   |");
+		System.out.println("  |  2   Pipoca Doce          R$ 12.00   |");
+		System.out.println("  |  3   Doritos              R$ 15.00   |");
+		System.out.println("  |  4   Bibs                 R$ 8.00    |");
+		System.out.println("  |  5   Guaraná Antarctica   R$ 6.00    |");
+		System.out.println("  |  6   Coca-cola            R$ 6.00    |");
+		System.out.println("  |  7   Água                 R$ 4.00    |");
+		System.out.println("  |______________________________________|");
+
+		double valorTotal = 0;
+
+		while (true) {
+			
+			System.out.println("Digite o número do alimento ou bebida que você deseja comprar (0 para encerrar): ");
+			int escolhaAlimento = entrada.nextInt();
+
+			if (escolhaAlimento == 0) {
+				break;
+			}
+
+			System.out.println("Digite a quantidade: ");
+			int quantidadeAlimento = entrada.nextInt();
+
+			switch (escolhaAlimento) {
+			case 1:
+				valorTotal += quantidadeAlimento * 10;
+				break;
+			case 2:
+				valorTotal += quantidadeAlimento * 12;
+				break;
+			case 3:
+				valorTotal += quantidadeAlimento * 15;
+				break;
+			case 4:
+				valorTotal += quantidadeAlimento * 8;
+				break;
+			case 5:
+			case 6:
+				valorTotal += quantidadeAlimento * 6;
+				break;
+			case 7:
+				valorTotal += quantidadeAlimento * 4;
+				break;
+			default:
+				System.out.println("Número inválido!");
+			}
 		}
-		this.bebida = bebida;
+
+		System.out.println("Total a pagar: R$" + valorTotal);
 	}
 
-	public boolean isPagamento() {
-		return pagamento;
-	}
-
-	public void setPagamento(boolean pagamento) {
-		this.pagamento = pagamento;
-	}
+	
 
 	@Override
 	public String pagar(boolean pago) {
 		if (pago) {
-			return "Pago";	
+			return "Pago";
 		} else {
 			return "Não Pago";
 		}
-		
+
 	}
 
 	@Override
 	public String toString() {
-		return "Alimento: " + this.alimento + " Bebida: " + " Pagamento: " + this.pagamento;
+		return "Pagamento: " + pagar(true);
 	}
 
 }
