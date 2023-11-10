431b05b3bebf9761feff160508f9b04b9591a184
ViniciusBlau <108106730+ViniciusBlau@users.noreply.github.com>
2023-11-09 22:37:21 -0300

    Update Lanchonete.java

--- TrabalhoJava/src/TrabalhoJava/Lanchonete.java
+++ TrabalhoJava/src/TrabalhoJava/Lanchonete.java
@@ -3,30 +3,30 @@ package TrabalhoJava;
 import java.util.Scanner;
 
 public class Lanchonete extends Estabelecimento {
-	Scanner entrada = new Scanner(System.in);
+    Scanner entrada = new Scanner(System.in);
 
-	private int funcionarios;
+    private int funcionarios;
+    private double valorTotal;
 
-	public Lanchonete(String formaPagamento, Endereco local, boolean funcionamento, int saidasEmergencia,
-			int funcionarios) {
-		super(formaPagamento, local, funcionamento, saidasEmergencia);
-		setFuncionarios(funcionarios);
-	}
+    public Lanchonete(String formaPagamento, Endereco local, boolean funcionamento, int saidasEmergencia,
+                      int funcionarios) {
+        super(formaPagamento, local, funcionamento, saidasEmergencia);
+        setFuncionarios(funcionarios);
+    }
 
-	public int getFuncionarios() {
-		return funcionarios;
-	}
+    public int getFuncionarios() {
+        return funcionarios;
+    }
 
-	public void setFuncionarios(int funcionarios) {
-		if (funcionarios < 1 || funcionarios > 15) {
-			throw new IllegalArgumentException("Funcionários inválidos!");
-		}
-		this.funcionarios = funcionarios;
-	}
+    public void setFuncionarios(int funcionarios) {
+        if (funcionarios < 1 || funcionarios > 15) {
+            throw new IllegalArgumentException("Funcionários inválidos!");
+        }
+        this.funcionarios = funcionarios;
+    }
 
-	public void comprar() {
-
-		System.out.println("  ________________________________________");
+    public void comprar() {
+    	System.out.println("  ________________________________________");
 		System.out.println("  |                                      |");
 		System.out.println("  |         Alimentos e Bebidas          |");
 		System.out.println("  |______________________________________|");
@@ -40,65 +40,60 @@ public class Lanchonete extends Estabelecimento {
 		System.out.println("  |  7   Água                 R$ 4.00    |");
 		System.out.println("  |______________________________________|");
 
-		double valorTotal = 0;
+        double valorTotal = 0;
 
-		while (true) {
-			
-			System.out.println("Digite o número do alimento ou bebida que você deseja comprar (0 para encerrar): ");
-			int escolhaAlimento = entrada.nextInt();
+        while (true) {
+            System.out.println("Digite o número do alimento ou bebida que você deseja comprar (0 para encerrar): ");
+            int escolhaAlimento = entrada.nextInt();
 
-			if (escolhaAlimento == 0) {
-				break;
-			}
+            if (escolhaAlimento == 0) {
+                break;
+            }
 
-			System.out.println("Digite a quantidade: ");
-			int quantidadeAlimento = entrada.nextInt();
+            System.out.println("Digite a quantidade: ");
+            int quantidadeAlimento = entrada.nextInt();
 
-			switch (escolhaAlimento) {
-			case 1:
-				valorTotal += quantidadeAlimento * 10;
-				break;
-			case 2:
-				valorTotal += quantidadeAlimento * 12;
-				break;
-			case 3:
-				valorTotal += quantidadeAlimento * 15;
-				break;
-			case 4:
-				valorTotal += quantidadeAlimento * 8;
-				break;
-			case 5:
-				valorTotal += quantidadeAlimento * 6;
-				break;
-			case 6:
-				valorTotal += quantidadeAlimento * 6;
-				break;
-			case 7:
-				valorTotal += quantidadeAlimento * 4;
-				break;
-			default:
-				System.out.println("Número inválido!");
-			}
-		}
+            switch (escolhaAlimento) {
+                case 1:
+                    valorTotal += quantidadeAlimento * 10;
+                    break;
+                case 2:
+                    valorTotal += quantidadeAlimento * 12;
+                    break;
+                case 3:
+                    valorTotal += quantidadeAlimento * 15;
+                    break;
+                case 4:
+                    valorTotal += quantidadeAlimento * 8;
+                    break;
+                case 5:
+                    valorTotal += quantidadeAlimento * 6;
+                    break;
+                case 6:
+                    valorTotal += quantidadeAlimento * 6;
+                    break;
+                case 7:
+                    valorTotal += quantidadeAlimento * 4;
+                    break;
+                default:
+                    System.out.println("Número inválido!");
+            }
+        }
+        this.valorTotal = valorTotal;
+        System.out.println("Total a pagar: R$" + valorTotal);
+    }
 
-		System.out.println("Total a pagar: R$" + valorTotal);
-	}
+    @Override
+    public String pagar(boolean pago) {
+        if (pago) {
+        	return "Pago";
+        } else {
+        	return "Não pago";
+        }
+    }
 
-	
-
-	@Override
-	public String pagar(boolean pago) {
-		if (pago) {
-			return "Pago";
-		} else {
-			return "Não Pago";
-		}
-
-	}
-
-	@Override
-	public String toString() {
-		return "Pagamento: " + pagar(true);
-	}
-
+    @Override
+    public String toString() {
+        return "Forma de pagamento: " + getFormaPagamento() + " - Total a pagar: R$" + valorTotal + " - Conta: " + pagar(true);
+    }
 }
